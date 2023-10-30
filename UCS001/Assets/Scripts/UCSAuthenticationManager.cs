8c0975a33dcfccc1bfe934e66d1a7620552aebc1
Steve Jakab <steve.jakab@unity3d.com>
2023-10-30 10:50:39 -0700

    hooking up events

--- UCS001/Assets/Scripts/UCSAuthenticationManager.cs
+++ UCS001/Assets/Scripts/UCSAuthenticationManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using Unity.Services.Authentication;
 using Unity.Services.PlayerAccounts;
@@ -7,16 +8,16 @@ using Unity.Services.Core;
 
 public class UCSAuthenticationManager : MonoBehaviour
 {
-    public Event UserAuthenticated;
-    public Event UserAuthenticationFailed;
-    public Event UserAuthenticationExpired;
-    public Event UserSignedOut;
-    public Event GoToEnterGame;
+    public SOEvent UserAuthenticatedAndSignedIn;
+    public SOEvent UserAuthenticationFailed;
+    public SOEvent UserAuthenticationExpired;
+    public SOEvent UserSignedOut;
+    public SOEvent GoToEnterGame;
 
     // Start is called before the first frame update
-    void Start()
+     async void Start()
     {
-        
+        await Initialize();
     }
 
     // Update is called once per frame
@@ -25,7 +26,7 @@ public class UCSAuthenticationManager : MonoBehaviour
         
     }
 
-    private async void Initialize()
+    private async Task Initialize()
     {
         await UnityServices.InitializeAsync();
 
@@ -45,5 +46,31 @@ public class UCSAuthenticationManager : MonoBehaviour
         await PlayerAccountService.Instance.StartSignInAsync();
     }
 
+    private void handleSignIn()
+    {
+        // Fire event for user authenticated.
+        UserAuthenticatedAndSignedIn?.TriggerEvent();
 
+        // Shows how to get a playerID
+        Debug.Log($"PlayerID: {AuthenticationService.Instance.PlayerId}");
+
+        // Shows how to get an access token
+        Debug.Log($"Access Token: {AuthenticationService.Instance.AccessToken}");
+    }
+
+    private void handleSignInFailure(RequestFailedException err)
+    {
+        UserAuthenticationFailed?.TriggerEvent();
+        Debug.LogError(err);
+    }
+
+    private void handleSignOut()
+    {
+        UserSignedOut?.TriggerEvent();
+    }
+
+    private void handleAuthExpired()
+    {
+        UserAuthenticationExpired?.TriggerEvent();
+    }
 }
